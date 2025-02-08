c78d3271d98bc34d515e710b2230fdcbb721a60c
rsayyed591 <rehansayyed591@gmail.com>
2025-02-08 14:31:45 +0530

    added all the routes and all 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
 // import Image from "next/image";
 
+import HeroGeometric from "@/components/hero-geometric";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8">
-      <h1 className="text-4xl font-bold mb-8">Welcome to AnnSampark</h1>
-      <a 
-      href="/ngo" 
-      className="rounded-full bg-foreground text-background px-6 py-3 text-lg hover:bg-[#383838] transition-colors"
-      >
-      Get Started
-      </a>
-    </div>
+    <HeroGeometric />
   );
 }
